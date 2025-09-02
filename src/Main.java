2db9bcef1ffeeca28ddf04fcbdc8f0ee651b46f5
AntND <antonlenz508@gmail.com>
2025-09-02 16:32:03 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import problems.NumJewelsInStones;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+    }
+}
---
+++ src/problems/NumJewelsInStones.java
@@ -0,0 +1,19 @@
+package problems;
+
+public class NumJewelsInStones {
+    public static int numJewelsInStones(String jewels, String stones) {
+
+        int ret = 0;
+
+        for (int i = 0 ; i < jewels.length() ; i++){
+            for (int j = 0 ; j < stones.length() ; j++){
+
+                if (jewels.charAt(i) == stones.charAt(j)){
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+
+}
