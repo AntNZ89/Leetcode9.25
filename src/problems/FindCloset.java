e1ef8512f1feee9eb3b9092d7ba3940159a90655
AntND <antonlenz508@gmail.com>
2025-09-04 20:25:33 +0200

    Initial Commit

---
+++ src/problems/FindCloset.java
@@ -0,0 +1,20 @@
+package problems;
+
+public class FindCloset {
+    public int findClosest(int x, int y, int z) {
+
+        int num1 = Math.abs(z-x);
+        int num2 = Math.abs(z-y);
+
+        if (num1 == num2){
+            return 0;
+        }
+        else if (num1 < num2){
+            return 1;
+        }
+        else {
+            return 2;
+        }
+
+    }
+}
