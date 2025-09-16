aed97f5bd63a898940abe7a432172ee00c04a1d0
AntND <antonlenz508@gmail.com>
2025-09-16 22:20:15 +0200

    Initial Commit

---
+++ src/problems/PlusOne.java
@@ -0,0 +1,65 @@
+package problems;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+        boolean bol = true;
+        int k = 1;
+        List<Integer> list = new LinkedList<>();
+        int[] digits2 = new int[digits.length+1];
+        boolean all9 = true;
+
+        while(bol){
+
+            for (int i = 0 ; i < digits.length ; i++){
+                if (digits[i] != 9){
+                    all9 = false;
+                    i = digits.length;
+                }
+            }
+
+            if (all9){
+                digits2[0] = 1;
+                for (int i = 1 ; i < digits2.length ; i++){
+                    digits2[i] = 0;
+                }
+                return digits2;
+            }
+            else if (digits[digits.length-k] != 9){
+                digits[digits.length-k]++;
+                return digits;
+            }
+            else if (digits[digits.length-k] == 9){
+                if (digits.length-k == 0){
+
+                    for (int i = 0 ; i < digits.length ; i++){
+                        list.add(digits[i]);
+                    }
+
+                    list.add(0, 1);
+
+                    for (int i = 0 ; i < list.size() ; i++){
+                        digits2[i] = list.get(i);
+                    }
+
+                    digits2[digits.length-k+1] = 0;
+
+                    return digits2;
+                }
+                else {
+
+                    digits[digits.length-k] = 0;
+
+                }
+            }
+
+            k++;
+        }
+
+        return digits;
+
+    }
+}
