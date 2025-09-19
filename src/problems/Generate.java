0adedb92a8b16c2c3cd73d01a1e5e60e3ed518b5
AntND <antonlenz508@gmail.com>
2025-09-20 01:24:15 +0200

    Initial Commit

---
+++ src/problems/Generate.java
@@ -0,0 +1,47 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> listList = new ArrayList<>(numRows);
+        List<Integer> intList = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        List<Integer> help = new ArrayList<>();
+
+
+        for (int i = 0 ; i < numRows ; i++){
+            if (i == 0){
+                intList.add(1);
+                listList.add(new ArrayList<>(intList));
+            }
+            else {
+                for (int k = 0 ; k <= i ; k++){
+                    if (k == 0 || k == i){
+                        tempList.add(1);
+                        if (k == i){
+                            intList.clear();
+                            intList.addAll(tempList);
+                            listList.add(new ArrayList<>(intList));
+                            tempList.clear();
+                        }
+                    }
+                    else {
+                        int num = intList.get(k-1) + intList.get(k);
+                        tempList.add(num);
+                        num = 0;
+                    }
+                }
+
+            }
+
+
+        }
+
+        return listList;
+
+
+    }
+}
