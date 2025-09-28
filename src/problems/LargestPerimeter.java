76219754d60bcecf8390fd462e28aa40dc061467
AntND <antonlenz508@gmail.com>
2025-09-28 16:31:24 +0200

    Initial Commit

--- src/problems/LargestPerimeter.java
+++ src/problems/LargestPerimeter.java
@@ -6,12 +6,11 @@ public class LargestPerimeter {
     public int largestPerimeter(int[] nums) {
 
         int n = nums.length;
-        int ans = 0;
         Arrays.sort(nums);
 
         for (int i = n-1 ; i >= 2 ; i--){
             if (nums[i-2] + nums[i-1] > nums[i]){
-                return nums[i] + nums[i-1] + nums[i-2];
+                return nums[i-2] + nums[i-1] + nums[i];
             }
         }
 
