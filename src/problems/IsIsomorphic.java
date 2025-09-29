b2e7864d4637eb240848e7fd57d73e2fc95615e5
AntND <antonlenz508@gmail.com>
2025-09-29 21:42:00 +0200

    Initial Commit

---
+++ src/problems/IsIsomorphic.java
@@ -0,0 +1,30 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class IsIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+
+        HashMap<Character, Character> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0 ; i < s.length() ; i++){
+            if (map.containsKey(s.charAt(i))){
+                if (map.get(s.charAt(i)) != t.charAt(i)){
+                    return false;
+                }
+            }
+            else if (!map.containsKey(s.charAt(i)) && set.contains(t.charAt(i))){
+                return false;
+            }
+            else {
+                map.put(s.charAt(i) , t.charAt(i));
+                set.add(t.charAt(i));
+            }
+        }
+
+        return true;
+
+    }
+}
